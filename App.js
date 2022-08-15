151d063f690dbee480e7326a7039981d50c255f5
Rakha112 <rakhawibowo1998@gmail.com>
2022-08-15 19:49:47 +0700

    First Commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Custom Check Box</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
