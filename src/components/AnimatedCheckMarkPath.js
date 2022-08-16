4c88afaff04cfc553b7bc01b375b9fc1de7cc222
Rakha112 <rakhawibowo1998@gmail.com>
2022-08-16 19:31:54 +0700

    Custom Checkbox

---
+++ src/components/AnimatedCheckMarkPath.js
@@ -0,0 +1,27 @@
+import React, {useRef, useState} from 'react';
+import Animated, {useAnimatedProps} from 'react-native-reanimated';
+import {Path} from 'react-native-svg';
+const AnimatedCheckMarkPath = ({progress, checkMarkColor}) => {
+  const [length, setLength] = useState(0);
+  const pathRef = useRef(null);
+  const AnimatedPath = Animated.createAnimatedComponent(Path);
+  const checkMarkAnimation = useAnimatedProps(() => {
+    const strokeDashoffset = length - length * progress.value;
+    return {strokeDashoffset};
+  });
+  return (
+    <AnimatedPath
+      animatedProps={checkMarkAnimation}
+      onLayout={() => setLength(pathRef.current.getTotalLength())}
+      ref={pathRef}
+      d="M12 24.4068L20.6667 32.9999L36.5 17.1667"
+      stroke={checkMarkColor}
+      strokeWidth="5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeDasharray={length}
+    />
+  );
+};
+
+export default AnimatedCheckMarkPath;
---
+++ src/components/CustomCheckBox.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import Svg from 'react-native-svg';
+import {useDerivedValue, withTiming} from 'react-native-reanimated';
+import AnimatedFrontPath from './AnimatedFrontPath';
+import AnimatedCheckMarkPath from './AnimatedCheckMarkPath';
+import AnimatedBackPath from './AnimatedBackPath';
+const CustomCheckBox = ({
+  checked,
+  checkMarkColor,
+  checkedBorderColor,
+  unCheckedBorderColor,
+  checkedBackgroundColor,
+  unCheckedBackgroundColor,
+}) => {
+  const pathProgress = useDerivedValue(() => {
+    return withTiming(checked ? 1 : 0);
+  });
+  const backgroundProgress = useDerivedValue(() => {
+    return withTiming(pathProgress.value === 1 ? 1 : 0);
+  });
+  const checkMarkProgress = useDerivedValue(() => {
+    return withTiming(pathProgress.value === 1 ? 1 : 0);
+  });
+
+  return (
+    <Svg width="49" height="49" viewBox="0 0 49 49">
+      <AnimatedBackPath
+        progress={backgroundProgress}
+        checkedBorderColor={checkedBorderColor}
+        checkedBackgroundColor={checkedBackgroundColor}
+        unCheckedBackgroundColor={unCheckedBackgroundColor}
+      />
+      <AnimatedCheckMarkPath
+        progress={checkMarkProgress}
+        checkMarkColor={checkMarkColor}
+      />
+      <AnimatedFrontPath
+        progress={pathProgress}
+        unCheckedBorderColor={unCheckedBorderColor}
+      />
+    </Svg>
+  );
+};
+
+export default CustomCheckBox;
