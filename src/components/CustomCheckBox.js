eb12ef3f344260bfcbf9d991b5dcaae1f16a435e
Rakha112 <rakhawibowo1998@gmail.com>
2022-08-17 20:11:43 +0700

    use Memo to prevent rerender

--- src/components/CustomCheckBox.js
+++ src/components/CustomCheckBox.js
@@ -1,45 +1,47 @@
-import React from 'react';
+import React, {memo} from 'react';
 import Svg from 'react-native-svg';
 import {useDerivedValue, withTiming} from 'react-native-reanimated';
 import AnimatedFrontPath from './AnimatedFrontPath';
 import AnimatedCheckMarkPath from './AnimatedCheckMarkPath';
 import AnimatedBackPath from './AnimatedBackPath';
-const CustomCheckBox = ({
-  checked,
-  checkMarkColor,
-  checkedBorderColor,
-  unCheckedBorderColor,
-  checkedBackgroundColor,
-  unCheckedBackgroundColor,
-}) => {
-  const pathProgress = useDerivedValue(() => {
-    return withTiming(checked ? 1 : 0);
-  });
-  const backgroundProgress = useDerivedValue(() => {
-    return withTiming(pathProgress.value === 1 ? 1 : 0);
-  });
-  const checkMarkProgress = useDerivedValue(() => {
-    return withTiming(pathProgress.value === 1 ? 1 : 0);
-  });
+const CustomCheckBox = memo(
+  ({
+    checked,
+    checkMarkColor,
+    checkedBorderColor,
+    unCheckedBorderColor,
+    checkedBackgroundColor,
+    unCheckedBackgroundColor,
+  }) => {
+    const pathProgress = useDerivedValue(() => {
+      return withTiming(checked ? 1 : 0);
+    });
+    const backgroundProgress = useDerivedValue(() => {
+      return withTiming(pathProgress.value === 1 ? 1 : 0);
+    });
+    const checkMarkProgress = useDerivedValue(() => {
+      return withTiming(pathProgress.value === 1 ? 1 : 0);
+    });
 
-  return (
-    <Svg width="49" height="49" viewBox="0 0 49 49">
-      <AnimatedBackPath
-        progress={backgroundProgress}
-        checkedBorderColor={checkedBorderColor}
-        checkedBackgroundColor={checkedBackgroundColor}
-        unCheckedBackgroundColor={unCheckedBackgroundColor}
-      />
-      <AnimatedCheckMarkPath
-        progress={checkMarkProgress}
-        checkMarkColor={checkMarkColor}
-      />
-      <AnimatedFrontPath
-        progress={pathProgress}
-        unCheckedBorderColor={unCheckedBorderColor}
-      />
-    </Svg>
-  );
-};
+    return (
+      <Svg width="49" height="49" viewBox="0 0 49 49">
+        <AnimatedBackPath
+          progress={backgroundProgress}
+          checkedBorderColor={checkedBorderColor}
+          checkedBackgroundColor={checkedBackgroundColor}
+          unCheckedBackgroundColor={unCheckedBackgroundColor}
+        />
+        <AnimatedCheckMarkPath
+          progress={checkMarkProgress}
+          checkMarkColor={checkMarkColor}
+        />
+        <AnimatedFrontPath
+          progress={pathProgress}
+          unCheckedBorderColor={unCheckedBorderColor}
+        />
+      </Svg>
+    );
+  },
+);
 
 export default CustomCheckBox;
