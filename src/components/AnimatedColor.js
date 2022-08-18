ccc3eae86544f8e0e1686ac39bd0c6a9a76816df
Rakha112 <rakhawibowo1998@gmail.com>
2022-08-18 17:08:12 +0700

    Change Variabel Name

--- src/components/AnimatedColor.js
+++ src/components/AnimatedColor.js
@@ -11,7 +11,7 @@ const AnimatedColor = ({
   unCheckedBackgroundColor,
   checkedBackgroundColor,
 }) => {
-  const AnimatedPath = Animated.createAnimatedComponent(Path);
+  const AnimationColor = Animated.createAnimatedComponent(Path);
   const animation = useAnimatedProps(() => {
     const fill = interpolateColor(
       progress.value,
@@ -26,7 +26,7 @@ const AnimatedColor = ({
     return {fill, stroke};
   });
   return (
-    <AnimatedPath
+    <AnimationColor
       animatedProps={animation}
       d="M2 16C2 8.26801 8.26801 2 16 2H33C40.732 2 47 8.26801 47 16V33C47 40.732 40.732 47 33 47H16C8.26801 47 2 40.732 2 33V16Z"
       strokeWidth={4}
